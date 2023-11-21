8db057e34a9d0910771193f128c96a28ef34f060
csae8092 <csae8092@users.noreply.github.com>
2023-11-21 15:45:53 +0000

    deploy: cab3f31196ee09157153b06a77bf460f68fbc107

--- tabulator-js/config.js
+++ tabulator-js/config.js
@@ -4,5 +4,8 @@ var config = {
     height: 800,
     layout: "fitColumns",
     tooltips: true,
-    dataLoader: true
-};
+    dataLoader: true,
+    pagination: "local",
+    paginationSize: 15,
+    paginationCounter: "rows"
+    };
