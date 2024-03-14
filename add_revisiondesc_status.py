35516ae7b14f330151a29ec4c0e9e15dd5f8417b
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-14 08:59:49 +0100

    added script to encoded Erschließungsstand  #56

---
+++ add_revisiondesc_status.py
@@ -0,0 +1,30 @@
+import glob
+from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import extract_fulltext
+from tqdm import tqdm
+from slugify import slugify
+
+files = sorted(glob.glob("./data/editions/*.xml"))
+print(f"adding revisionDesc @status and @n values for {len(files)} documents")
+
+for x in tqdm(files):
+    doc_status = ""
+    doc = TeiReader(x)
+    fulltext = extract_fulltext(doc.any_xpath(".//tei:body")[0])
+    if fulltext:
+        doc_status += "Volltext erschlossen; "
+    rs_tags = doc.any_xpath(".//tei:body//tei:rs")
+    if rs_tags:
+        doc_status += "Entitäten ausgezeichnet; "
+    regest = extract_fulltext(doc.any_xpath(".//tei:abstract[@n='regest']")[0])
+    if regest:
+        doc_status += "Regestvorhanden; "
+    if not doc_status:
+        doc_status = "nicht weiter erschlossen;"
+    doc_status = doc_status.strip()
+    if doc_status[-1] == ";":
+        doc_status = doc_status[:-1]
+    revision_desc = doc.any_xpath(".//tei:revisionDesc")[0]
+    revision_desc.attrib["status"] = slugify(doc_status)
+    revision_desc.attrib["n"] = doc_status
+    doc.tree_to_file(x)
--- process.sh
+++ process.sh
@@ -4,6 +4,7 @@ add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at.at/emt"
 denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref | .//*/@source" -x ".//tei:titleStmt/tei:title[@type='main']/text()"
 python rm_listevent.py
 python add_correspContext.py
+python add_revisiondesc_status.py
 python make_typesense_index.py
 python make_calendar_data.py
 python make_translations.py
