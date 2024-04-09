f684448af20eb8ba300b82e455cd482e69e7d8de
csae8092 <csae8092@users.noreply.github.com>
2024-04-09 10:43:58 +0000

    deploy: cc9222a60231750c768eeb4efdc6b9ec724cce85

--- js/i18n.js
+++ js/i18n.js
@@ -1,8 +1,3 @@
-const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' },
-};
-
 const rerender = () => {
   $('body').localize();
 }
@@ -25,31 +20,6 @@ $(function () {
     }, (err, t) => {
       if (err) return console.error(err);
       jqueryI18next.init(i18next, $, { useOptionsAttr: true });
-      Object.keys(lngs).map((lng) => {
-        const opt = new Option(lngs[lng].nativeName, lng);
-        if (lng === i18next.resolvedLanguage) {
-          opt.setAttribute("selected", "selected");
-        }
-        $('#languageSwitcher').append(opt);
-      });
-      $('#languageSwitcher').change((a, b, c) => {
-        const chosenLng = $(this).find("option:selected").attr('value');
-        var hide_this_lang = (chosenLng === "en") ? "de" : "en";
-        var show_this_lang = chosenLng
-        var to_show = document.querySelectorAll(`[data-mylang="${show_this_lang}"]`);
-        to_show.forEach(function (el) {
-          el.style.removeProperty("display")
-        });
-        var to_hide = document.querySelectorAll(`[data-mylang="${hide_this_lang}"]`);
-        console.log(`hide ${hide_this_lang}`)
-        to_hide.forEach(function (el) {
-          el.style.display = 'none';
-        });
-        i18next.changeLanguage(chosenLng, () => {
-          rerender();
-        });
-      });
-
       rerender();
     });
 });
