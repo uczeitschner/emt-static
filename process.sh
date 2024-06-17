822786156e20da595d805c6910b615c40bd6e218
csae8092 <peter.andorfer@oeaw.ac.at>
2024-06-17 17:47:07 +0200

    minor

--- process.sh
+++ process.sh
@@ -6,6 +6,7 @@ python rm_listevent.py
 python add_correspContext.py
 python add_revisiondesc_status.py
 python make_calendar_data.py
+python cal_data.py
 python make_cmif.py
 python make_translations.py
 # python make_arche_rdf.py
