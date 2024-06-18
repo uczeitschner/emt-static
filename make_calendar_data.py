b1ba4a89118fb96fed12b1acfb6061677cd7f64a
csae8092 <peter.andorfer@oeaw.ac.at>
2024-06-18 09:19:55 +0200

    replaces old calendar

--- make_calendar_data.py
+++ make_calendar_data.py
@@ -1,86 +1,72 @@
-import csv
-import glob
-import os
 import json
+import os
+import glob
+import pandas as pd
 from acdh_tei_pyutils.tei import TeiReader
-from tqdm import tqdm
+from acdh_tei_pyutils.utils import normalize_string
+from acdh_cidoc_pyutils import extract_begin_end
+
 
 file_list = sorted(glob.glob("./data/editions/*.xml"))
 data_dir = os.path.join("html", "js-data")
 os.makedirs(data_dir, exist_ok=True)
-out_file = os.path.join(data_dir, "calendarData.js")
-
-no_dates = []
-data = []
+out_file = os.path.join(data_dir, "calendarData.json")
 broken = []
-for file_name in tqdm(file_list, total=len(file_list)):
-    doc = TeiReader(file_name)
-    head, tail = os.path.split(file_name)
-    id = tail.replace(".xml", "")
+events = []
+sender = set()
+for x in file_list:
+    f_id = os.path.split(x)[1].replace(".xml", ".html")
+    item = {
+        "link": f_id
+    }
+    doc = TeiReader(x)
+    title_node = doc.any_xpath(".//tei:fileDesc/tei:titleStmt/tei:title[@type='main'][1]//text()")[0]
 
-    # correspAction/date
     try:
         sent_date_node = doc.any_xpath("//tei:correspAction/tei:date")[0]
     except IndexError:
-        broken.append(f"missing '//tei:correspAction/tei:date' in file: {file_name}")
+        broken.append(f"missing '//tei:correspAction/tei:date' in file: {x}")
         continue
-    is_valid_date = False
-    if "when-iso" in sent_date_node.attrib:
-        ca_date_when = sent_date_node.attrib["when-iso"]
-        is_valid_date = True
-    elif "when" in sent_date_node.attrib:
-        ca_date_when = sent_date_node.attrib["when"]
-        is_valid_date = True
-    else:
-        no_dates.append(tail)
-
-    if is_valid_date:
-        item = {
-            "id": id + ".html",
-            "name": doc.any_xpath("//tei:title[@type='main']/text()")[0],
-            "date": ca_date_when,
-        }
-        data.append(item)
-
-    # body/date
-    body_dates = doc.any_xpath("//tei:body//tei:date[@type='letter']")
-    for body_date_node in body_dates:
-        if "when-iso" in body_date_node.attrib:
-            body_date_when = body_date_node.attrib["when-iso"]
-        elif "when" in body_date_node.attrib:
-            body_date_when = body_date_node.attrib["when"]
+    dates = extract_begin_end(sent_date_node)
+    if dates[0]:
+        item["date"] = dates[0]
+        item["from"] = dates[0]
+        item["to"] = dates[1]
+        if dates[0] == dates[1]:
+            item["range"] = False
         else:
-            print(f"{id}: invalid date node in body", body_date_node.attrib)
-            continue
-        if (
-            is_valid_date and body_date_when == ca_date_when
-        ):  # ignore in-body authoring date
-            continue
-
-        body_date_item = {
-            "name": "Brief erschlossen",
-            "date": body_date_when,
-            "id": False,
-            "ref_by_id": id,
-            "ref_by_date": ca_date_when if is_valid_date else None,
+            item["range"] = True
+        item["label"] = normalize_string(title_node)
+        sender_name = normalize_string(doc.any_xpath(".//tei:correspAction[@type='sent']/tei:persName//text()")[0])
+        sender_id = doc.any_xpath(".//tei:correspAction[@type='sent']/tei:persName/@ref")[0]
+        item["kind"] = f"{sender_id[1:]}.html"
+        item["sender"] = {
+            "label": sender_name,
+            "link": f"{sender_id[1:]}.html"
         }
-        data.append(body_date_item)
+        events.append(item)
+    else:
+        pass
+    for y in doc.any_xpath("//tei:body//tei:date[@type='letter']"):
+        extra_item = {
+            "link": False,
+            "label": "Brief_erschlossen"
+        }
+        ex_date = extract_begin_end(y)
+        extra_item["date"] = ex_date[0]
+        extra_item["kind"] = "Brief_erschlossen"
+        extra_item["from"] = ex_date[0]
+        extra_item["to"] = ex_date[1]
+        extra_item["ref_by"] = f_id
+        extra_item["sender"] = {
+            "label": "Brief erschlossen",
+            "link": "brief_erschlossen.html"
+        }
+        events.append(extra_item)
+
+with open(out_file, "w", encoding="utf-8") as f:
+    json.dump(events, f, ensure_ascii=False, indent=2)
 
 
-print(f"{len(data)} Datumsangaben aus {len(file_list)} Dateien extrahiert")
-
-print(f"writing calendar data to {out_file}")
-with open(out_file, "w", encoding="utf8") as f:
-    my_js_variable = f"var calendarData = {json.dumps(data, ensure_ascii=False)}"
-    f.write(my_js_variable)
-
-no_dates_file = "./html/no_dates.csv"
-print(f"writing files without date to {no_dates_file}")
-
-with open(no_dates_file, "w", newline="") as csvfile:
-    my_writer = csv.writer(csvfile, delimiter=",")
-    my_writer.writerow(["file_name"])
-    for fail_name in no_dates:
-        my_writer.writerow([fail_name])
-for x in broken:
-    print(x)
+df = pd.DataFrame(events)
+df.to_csv("hansi.csv", index=False)
